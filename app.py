ce76202983d356874d72a8a6c3c2b1b29582ca0f
Timothy Chang <76604824+timwonderer@users.noreply.github.com>
2025-06-20 19:35:19 -0700

    increase password_hash column type to Text for admins

--- app.py
+++ app.py
@@ -166,7 +166,7 @@ class Admin(db.Model):
     __tablename__ = 'admins'
     id             = db.Column(db.Integer, primary_key=True)
     username       = db.Column(db.String(80), unique=True, nullable=False)
-    password_hash  = db.Column(db.String(128), nullable=False)
+    password_hash  = db.Column(db.Text, nullable=False)
 
     @staticmethod
     def hash_password(pw: str) -> str:
